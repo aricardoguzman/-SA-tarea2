3230afc4198295aef04825cba789b5e53e8765a2
aricardoguzman <201010425@ingenieria.usac.edu.gt>
2020-03-07 00:54:48 -0600

    index added

--- app.js
+++ app.js
@@ -10,20 +10,33 @@ app.use('/static', express.static(path.join(__dirname, '/public')))
 const router = express.Router()
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.write(`<h1>Descargar build!</h1>
-    <a href="/.netlify/functions/app/version" download>Descargar archivo</a>`)
+  res.write(`
+    <style>
+      .button {
+        display: block;
+        width: 115px;
+        height: 25px;
+        background: #4E9CAF;
+        padding: 10px;
+        text-align: center;
+        border-radius: 5px;
+        color: white;
+        font-weight: bold;
+      }
+    </style>
+    <h1>Descargar build!</h1>
+    <a class="button" href="/.netlify/functions/app/version" download>Descargar archivo</a>`)
   res.end()
 })
 
-router.get('/version', (req, res) => {  
-  fs.readFile(path.join(__dirname, '/dist/release.zip'), (err, data) => {
-    if (err) {
+router.get('/version.zip', (req, res) => {
+  const readStream = fs.createReadStream('./dist/release.zip')
 
-    } else {
-      res.setHeader('Content-Type', 'application/x-gzip')
-      res.end(data)
-    }
-  })
+  // When everything has been read from the stream, end the response
+  readStream.on('close', () => res.end())
+
+  // Pipe the contents of the readStream directly to the response
+  readStream.pipe(res)
 })
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }))
