2a17af135de1d6da349c1a9334e397ac3dfeb286
aricardoguzman <201010425@ingenieria.usac.edu.gt>
2020-03-06 22:46:35 -0600

    changes to gulp file

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict'
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+
+app.use('/static', express.static(path.join(__dirname, '/public')))
+
+const router = express.Router()
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(`<h1>Descargar build!</h1>
+    <a href="/.netlify/functions/app/version" download>Descargar v1.2.0</a>`)
+  res.end()
+})
+
+router.get('/version', (req, res) => {  
+  fs.readFile(path.join(__dirname, '/dist/archive.tar.gz'), (err, data) => {
+    if (err) {
+
+    } else {
+      res.setHeader('Content-Type', 'application/x-gzip')
+      res.end(data)
+    }
+  })
+})
+
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }))
+
+app.use('/.netlify/functions/app', router) // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')))
+
+module.exports = app
+module.exports.handler = serverless(app)
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
-const gulp = require('gulp');
-const tar = require('gulp-tar');
-const gzip = require('gulp-gzip');
+const { series, src, dest } = require('gulp')
+const del = require('del')
+const zip = require('gulp-zip')
 
-gulp.task('runUploader', async function () {
-    gulp.src('./src/**')
-        .pipe(tar('archive.tar'))
-        .pipe(gzip())
-        .pipe(gulp.dest('./dist')) 
-});
+function clean () {
+  return del('/dist/**')
+}
+
+function build () {
+  return src(['*.js', '!gulpfile.js'])
+    .pipe(zip('release.zip'))
+    .pipe(dest('dist'))
+}
+/*
+function commit () {
+  return src('dist/*')
+    .pipe(git.add())
+    .pipe(git.commit('zipped micro'))
+}
+
+// push to release branch the commit made by commit task
+function push () {
+  return src('dist/*')
+    .pipe(git.push('origin', 'release'))
+}
+
+// makes a tag with the version in semver standard
+function tag () {
+  return src('./src/*.js')
+    .pipe(version(__dirname, './package.json'))
+    .pipe(dest('./dist'))
+    .pipe(push('origin', 'release', { args: '--tags' }))
+}
+*/
+
+exports.default = series(clean, build)
